f2f4ba03e3f4dee333ec13d050b2336c391ca835
Francis Chuang <francischuang@apache.org>
2018-04-17 10:41:59 +1000

    Remove go-cleanhttp

--- http_client.go
+++ http_client.go
@@ -22,12 +22,14 @@ import (
 	"database/sql/driver"
 	"fmt"
 	"io/ioutil"
+	"net"
 	"net/http"
 	"regexp"
+	"runtime"
+	"time"
 
 	avaticaMessage "github.com/apache/calcite-avatica-go/message"
 	"github.com/golang/protobuf/proto"
-	"github.com/hashicorp/go-cleanhttp"
 	"github.com/xinsnake/go-http-digest-auth-client"
 	"golang.org/x/net/context"
 	"golang.org/x/net/context/ctxhttp"
@@ -72,13 +74,25 @@ func (e avaticaError) Error() string {
 // NewHTTPClient creates a new httpClient from a host.
 func NewHTTPClient(host string, authenticationConf httpClientAuthConfig) (*httpClient, error) {
 
-	hc := cleanhttp.DefaultPooledClient()
-
 	c := &httpClient{
 		host:       host,
 		authConfig: authenticationConf,
 
-		httpClient: hc,
+		httpClient: &http.Client{
+			Transport: &http.Transport{
+				Proxy: http.ProxyFromEnvironment,
+				DialContext: (&net.Dialer{
+					Timeout:   30 * time.Second,
+					KeepAlive: 30 * time.Second,
+					DualStack: true,
+				}).DialContext,
+				MaxIdleConns:          100,
+				IdleConnTimeout:       90 * time.Second,
+				TLSHandshakeTimeout:   10 * time.Second,
+				ExpectContinueTimeout: 1 * time.Second,
+				MaxIdleConnsPerHost:   runtime.GOMAXPROCS(0) + 1,
+			},
+		},
 	}
 
 	if authenticationConf.authenticationType == digest {
